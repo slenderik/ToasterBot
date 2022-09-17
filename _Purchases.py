203da98a373e247c100abc1b70dd6527916fe481
slenderik <96630654+slenderik@users.noreply.github.com>
2022-09-17 19:01:23 +0600

    Update _Purchases.py

--- _Purchases.py
+++ _Purchases.py
@@ -22,6 +22,7 @@ class PurchasesCog(commands.Cog):
         ----------
         никнейм: Введите никнейм игрока, покупки которого хотите посмотреть
         """
+        print(inter.application_id)
         nickname = str(никнейм)
         nicknames = split(" |, | ,|,", nickname)
         purchase_embed = Embed(title=":shopping_bags: Покупки")
