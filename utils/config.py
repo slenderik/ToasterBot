adb8142d49ca121ca717b7fdaa4d002921eebad5
slenderik <slenderik2@gmail.com>
2022-10-16 20:23:17 +0600

    upd

--- utils/config.py
+++ utils/config.py
@@ -6,3 +6,4 @@ afk_channel_id = 1013422083134472243
 voice_create_channel_id = 997851742475669594
 logo_url = "https://media.discordapp.net/attachments/925973441524424716/925973455919251536/logo_bread.png?width=473" \
            "&height=473 "
+#logo_url = "https://media.discordapp.net/attachments/925973441524424716/925973455919251536/logo_bread.png"
