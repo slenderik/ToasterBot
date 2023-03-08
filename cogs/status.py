bb682c7f6df656cb96c8565758692c16d25fb39d
slenderik <slenderik2@gmail.com>
2023-03-08 11:42:27 +0600

    Привожу в порядок штуки

--- cogs/status.py
+++ cogs/status.py
@@ -1,6 +1,29 @@
 from datetime import datetime
 from disnake.ext import commands
+from mcstatus import
 
+voice_channel_id = 1066055695914520606
+text_channel_id = 1078357675622023228
+
+
+async def update_voice_online(bot):
+    try:
+        voice_channel = await bot.fetch_channel(voice_channel_id)
+
+        from mcstatus import BedrockServer
+
+        # You can pass the same address you'd enter into the address field in minecraft into the 'lookup' function
+        # If you know the host and port, you may skip this and use BedrockServer("example.org", 19132)
+        server = BedrockServer.lookup("example.org:19132")
+
+        # 'status' is the only feature that is supported by Bedrock at this time.
+        # In this case status includes players_online, latency, motd, map, gamemode, and players_max. (ex: status.gamemode)
+        status = server.status()
+        print(f"The server has {status.players.online} players online and replied in {status.latency} ms")
+
+        await
+    except:
+        ...
 
 class StatusCog(commands.Cog):
     """Команда статуса"""
