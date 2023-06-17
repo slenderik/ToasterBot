ea6322c5b97fac6c4823cc7a2cce9dcb18e89e2e
slenderik <slenderik2@gmail.com>
2023-06-17 12:06:42 +0600

    Mod Mail dm -> forum forum -> dm 1 event (half work)

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -13,87 +13,83 @@ class ModMail(Cog):
     async def on_ready(self):
         print(f"{self.bot.user} | {__name__}")
 
-    # FORUM -> DM
     @commands.Cog.listener()
     async def on_message(self, message: Message):
-        print("ANIME ", message.channel.id == modmail_forum_id)
-        if not message.channel.id == modmail_forum_id:
+
+        if message.author.id == self.bot.user.id:
             return
 
-        thread = message.thread
+        # FORUM -> DM
+        if message.channel.parent_id == modmail_forum_id:
 
-        user_id = thread.name[-19:-1]
+            thread = message.channel
 
-        try:
-            user = self.bot.get_user(user_id)
+            user_id = thread.name[-19:-1]
+
+            try:
+                user = await self.bot.fetch_user(user_id)
+
+                files = []
+
+                for attachment in message.attachments:
+                    files += await attachment.to_file(description=f"From {user.name} ({user.id})")
+
+                await user.send(
+                    files=files,
+                    embeds=message.embeds,
+                    content=message.content,
+                    stickers=message.stickers,
+                    components=message.components
+                )
+
+            except Exception as e:
+                await thread.send(f"ERROR: {e} <@{user_id}>")
+                await message.add_reaction("⚠️")
+
+            else:
+                await message.add_reaction("✔️")
+
+        # DM -> FORUM
+        if isinstance(message.channel, DMChannel):
+
+            modmail_forum: Thread
+            modmail_forum = self.bot.get_channel(modmail_forum_id)
+            user = message.author
+
+            if modmail_forum is None:
+                return
+
+            thread = None
+            for mod_thread in modmail_forum.threads:
+                print("name: ", mod_thread.name)
+                print("name: ", str(user.id) in mod_thread.name)
+                print("actived?: ", not mod_thread.archived)
+                print("Good one?: ", str(user.id) in mod_thread.name and not mod_thread.archived)
+
+                if (str(user.id) in mod_thread.name) and (not mod_thread.archived):
+                    thread = mod_thread
+                    print(thread)
+                    break
+
+            print(thread)
+            print("Create new: ", thread is not None)
+
+            if thread is None:
+                thread, message = await modmail_forum.create_thread(name=f"{user.name} ({user.id})", content=message.content)
+                print(thread.name)
 
             files = []
 
             for attachment in message.attachments:
                 files += await attachment.to_file(description=f"From {user.name} ({user.id})")
 
-            await user.send(
-                files=files,
-                embeds=message.embeds,
+            await thread.send(
                 content=message.content,
+                embeds=message.embeds,
+                files=files,
                 stickers=message.stickers,
                 components=message.components
             )
-
-        except Exception as e:
-            await thread.send(f"ERROR: {e}")
-            await message.add_reaction("⚠️")
-
-        else:
-            await message.add_reaction("✔️")
-
-
-
-
-    # DM -> FORUM
-    @commands.Cog.listener()
-    async def on_message(self, message: Message):
-        if not isinstance(message.channel, DMChannel):
-            return
-
-        modmail_forum: Thread
-        modmail_forum = self.bot.get_channel(modmail_forum_id)
-        user = message.author
-
-        if modmail_forum is None:
-            return
-
-        thread = None
-        for mod_thread in modmail_forum.threads:
-            print("name: ", mod_thread.name)
-            print("name: ", str(user.id) in mod_thread.name)
-            print("actived?: ", not mod_thread.archived)
-            print("Good one?: ", str(user.id) in mod_thread.name and not mod_thread.archived)
-
-            if (str(user.id) in mod_thread.name) and (not mod_thread.archived):
-                thread = mod_thread
-                print(thread)
-                break
-
-        print(thread)
-        print("Create new: ", thread is not None)
-
-        if thread is None:
-            thread, message = await modmail_forum.create_thread(name=f"{user.name} ({user.id})", content=message.content)
-            print(thread.name)
-
-        files = []
-
-        for attachment in message.attachments:
-            files += await attachment.to_file(description=f"From {user.name} ({user.id})")
-
-        await thread.send(
-            content=message.content,
-            embeds=message.embeds,
-            files=files,
-            stickers=message.stickers,
-            components=message.components
-        )
 
 
 
