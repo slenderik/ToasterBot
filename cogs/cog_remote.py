e26af26690fbc671a582910c728446b5dfc2b5f1
slenderik <slenderik2@gmail.com>
2023-06-26 17:26:44 +0700

    Errors excep added

--- cogs/cog_remote.py
+++ cogs/cog_remote.py
@@ -2,10 +2,10 @@ import os
 
 from disnake import Embed, ApplicationCommandInteraction
 from disnake.ext import commands
-from disnake.ext.commands import Bot, Cog
+from disnake.ext.commands import Bot, Cog, ExtensionAlreadyLoaded, NoEntryPointError, ExtensionFailed, ExtensionNotFound
 
 
-class RemoteCogsCog(Cog):
+class CogRemote(Cog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
@@ -40,21 +40,36 @@ class RemoteCogsCog(Cog):
             if filename.endswith(".py"):
                 # add to list all extension
                 extension_list += f"{filename[:-3]}, \n"
-        cog_list_embed = Embed(title="Список дополнений", description=extension_list)
+        cog_list_embed = Embed(title="Модули:", description=extension_list)
         await inter.response.send_message(embed=cog_list_embed, ephemeral=True)
 
     @cog.sub_command(name="загрузить")
     @commands.is_owner()
-    async def load(self, inter: ApplicationCommandInteraction, extension: cogs):
+    async def cog_load(self, inter: ApplicationCommandInteraction, extension: cogs):
         """Загрузить дополнение"""
-        print(f"[{inter.user.name}]: загружает {extension}")
-        self.bot.load_extension(extension)
-        cog_load_embed = Embed(title=f"Дополнение {extension} загружено!")
-        await inter.response.send_message(embed=cog_load_embed, ephemeral=True)
+        print(f"[{__name__}] {inter.user.name}: загружает {extension}")
+        try:
+            self.bot.load_extension(extension)
+
+        except ExtensionNotFound:
+            print(f"ERROR IN [{__name__}] {inter.user.name}: {extension} не найден.")
+            await inter.send(f"**Ошибка** `{extension}` не найден.", ephemeral=True)
+        except ExtensionAlreadyLoaded:
+            print(f"ERROR IN [{__name__}] {inter.user.name}: {extension} уже загружен.")
+            await inter.send(f"**Ошибка** `{extension}` уже загружен. Для начала выгрузите его или перезагрузите.", ephemeral=True)
+        except NoEntryPointError:
+            print(f"ERROR IN [{__name__}] {inter.user.name}: В {extension} нет setup()!")
+            await inter.send(f"**Ошибка** В `{extension}` нет setup() функции.", ephemeral=True)
+        except ExtensionFailed:
+            print(f"ERROR IN [{__name__}] {inter.user.name}: Ошибка в {extension}!")
+            await inter.send(f"**Ошибка** Ошибка в `{extension}` при загрузке.", ephemeral=True)
+
+        else:
+            await inter.response.send_message(f"✔ {extension} загружен!", ephemeral=True)
 
     @cog.sub_command(name="выгрузить")
     @commands.is_owner()
-    async def unload(self, inter: ApplicationCommandInteraction, extension: cogs):
+    async def cog_unload(self, inter: ApplicationCommandInteraction, extension: cogs):
         """Выгрузить дополнение"""
         print(f"[{inter.user.name}]: выгружает {extension}")
         self.bot.unload_extension(extension)
@@ -63,7 +78,7 @@ class RemoteCogsCog(Cog):
 
     @cog.sub_command(name="перезагрузить")
     @commands.is_owner()
-    async def creload(self, inter: ApplicationCommandInteraction, extension: cogs):
+    async def cog_reload(self, inter: ApplicationCommandInteraction, extension: cogs):
         """Загрузить дополнение"""
         print(f"[{inter.user.name}]: перезагружает {extension}")
         self.bot.reload_extension(extension)
@@ -72,7 +87,7 @@ class RemoteCogsCog(Cog):
 
 
 def setup(bot: Bot) -> None:
-    bot.add_cog(RemoteCogsCog(bot))
+    bot.add_cog(CogRemote(bot))
     print(f" + {__name__}")
 
 
